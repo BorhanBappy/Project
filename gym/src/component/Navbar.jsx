2a18ef63ab0b08021655d5873b32450fe89b8379
BorhanBappy <bappyzx1@gmail.com>
2023-09-04 10:35:11 +0600

    Navbarup

--- gym/src/component/Navbar.jsx
+++ gym/src/component/Navbar.jsx
@@ -22,49 +22,40 @@ export default function Navbar() {
   };
 
   return (
-    <>
-      
-          <div className="shadow-md  h- w-full top-0 left-0 bg-white">
-            <div className="flex items-center justify-between py-4">
-              <div className="flex items-center">
-                <span className="text-3xl text-indigo-600 mr-1 ">
-                  <MdSportsGymnastics />
-                </span>
-                <span className="font-bold text-2xl cursor-pointer font-sans">
-                  Borhan's GYM
-                </span>
-              </div>
-              <div className="md:float-right static relative mr-2 mb-12">
-                {/* Add a button for medium screens */}
-                {window.innerWidth <= 768 && (
-                  <button
-                    onClick={toggleNav}
-                    className="md:hidden block text-gray-80 float-right mr-3"
-                  >
-                    {isNavOpen ? <FaBars /> : <FaBars />}
-                  </button>
-                )}
-                {/* Navigation menu */}
-                <ul
-                  className={`md:flex md:items-center ${
-                    isNavOpen ? "block" : "hidden"
-                  } absolute top-full right-0 bg-white mt-1 p-2 `}
+    <div className="w-full top-0 left-0 bg-slate-400">
+      <div className="flex items-center justify-between ">
+        <div className="flex items-center">
+          <span className="text-3xl text-indigo-600">
+            <MdSportsGymnastics />
+          </span>
+          <span className="font-bold md:text-2xl cursor-pointer font-sans">
+            Borhan&apos;s GYM
+          </span>
+        </div>
+        <div className="relative mr-2 sm:mb-12">
+          <button onClick={toggleNav} className="sm:hidden block ">
+            <FaBars />
+          </button>
+
+          {/* Navigation menu */}
+          <ul
+            className={`sm:flex sm:items-center ${
+              isNavOpen ? "block" : "hidden"
+            } absolute top-full right-0 bg-slate-400  p-2 `}
+          >
+            {Navlinks.map((linkItem, index) => (
+              <li key={index} className="md:ml-3 text-xl mr-4">
+                <Link
+                  to={linkItem.link}
+                  className="text-gray-800 text-2xl  hover:text-blue-800 duration-500  md:space-x-12"
                 >
-                  {Navlinks.map((linkItem, index) => (
-                    <li key={index} className="md:ml-3 text-xl mr-4">
-                      <Link
-                        to={linkItem.link}
-                        className="text-gray-800 hover:text-blue-700 duration-500  hover:bg-slate-300 md:space-x-12"
-                      >
-                        {linkItem.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-      
-    </>
+                  {linkItem.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
